31f2c19efe825cd1c5cfe7b298a752995ad4f765
Ami Fukuyama <amifuku80@gmail.com>
2026-01-15 13:23:43 +0800

    Added session on App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,23 @@
+import React, { useEffect, useState } from "react";
 import Note from "./note.jsx";
 import { Auth } from "./auth.jsx";
+import { supabase } from "./supabase-client";
 
 import "./App.css";
 
 function App() {
+  const [session, setSession] = useState(null);
+
+  const fetchSession = async () => {
+    const currentSession = await supabase.auth.getSession();
+    console.log(currentSession);
+    setSession(currentSession.data);
+  };
+
+  useEffect(() => {
+    fetchSession();
+  }, []);
+
   return (
     <div>
       <Note />
